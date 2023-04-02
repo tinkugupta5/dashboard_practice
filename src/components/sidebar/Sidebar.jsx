7565552c1d36f016a2770a5fcfd8d904b34fb99c
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-04-03 00:07:45 +0530

    adding sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,38 @@ import React from "react";
 import "./sidebar.scss";
 
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">Dashboard</span>
+      </div>
+      <hr />
+
+      <div className="center">
+        <ul>
+          <li>
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <span></span>
+          </li>
+          <li>
+            <span></span>
+          </li>
+          <li>
+            <span></span>
+          </li>
+          <li>
+            <span></span>
+          </li>
+          <li>
+            <span></span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">Color Option</div>
+    </div>
+  );
 };
 
 export default Sidebar;
