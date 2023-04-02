39e53f54d219a772c9089d2759625e7e8806e25a
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-04-02 23:02:30 +0530

    adding dashboard

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Home;
