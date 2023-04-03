5e0a220baf7e0d4f9d78b0ae58a8e2c35d96e6af
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-04-04 00:41:51 +0530

    adding sidebar icons and styling

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonAddAltTwoToneIcon from "@mui/icons-material/PersonAddAltTwoTone";
+import Inventory2TwoToneIcon from "@mui/icons-material/Inventory2TwoTone";
+import LogoutSharpIcon from "@mui/icons-material/LogoutSharp";
+import QueryStatsSharpIcon from "@mui/icons-material/QueryStatsSharp";
+import CircleNotificationsSharpIcon from "@mui/icons-material/CircleNotificationsSharp";
+import SettingsSystemDaydreamSharpIcon from "@mui/icons-material/SettingsSystemDaydreamSharp";
+import HealthAndSafetySharpIcon from "@mui/icons-material/HealthAndSafetySharp";
+import SettingsApplicationsSharpIcon from "@mui/icons-material/SettingsApplicationsSharp";
+import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
 
 const Sidebar = () => {
   return (
@@ -11,23 +21,54 @@ const Sidebar = () => {
 
       <div className="center">
         <ul>
+          <p className="title">Main</p>
           <li>
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">Lists</p>
           <li>
-            <span></span>
+            <PersonAddAltTwoToneIcon className="icon" />
+            <span>Users</span>
           </li>
           <li>
-            <span></span>
+            <Inventory2TwoToneIcon className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span></span>
+            <PersonAddAltTwoToneIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">Useful</p>
+          <li>
+            <QueryStatsSharpIcon className="icon" />
+            <span>Stats</span>
           </li>
           <li>
-            <span></span>
+            <CircleNotificationsSharpIcon className="icon" />
+            <span>Notification</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamSharpIcon className="icon" />
+            <span>System Health</span>
           </li>
           <li>
-            <span></span>
+            <HealthAndSafetySharpIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsSharpIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">User</p>
+          <li>
+            <AccountCircleSharpIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutSharpIcon className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
