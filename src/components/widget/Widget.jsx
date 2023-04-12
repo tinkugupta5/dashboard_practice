55c542fd5e1044b674569e8bf9208168ec417cf7
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-04-12 23:05:42 +0530

    adding widget

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "./widget.scss";
 
 const Widget = () => {
   return (
-    <div>
-      <h1>This</h1>
+    <div className="widget">
+      <div className="left">Left</div>
+      <div className="left">Right</div>
     </div>
   );
 };
