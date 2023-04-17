82838cecab253db17785632ea5c37359d2ae8a71
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-04-17 23:11:21 +0530

    adding title

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -4,7 +4,10 @@ import "./widget.scss";
 const Widget = () => {
   return (
     <div className="widget">
-      <div className="left">Left</div>
+      <div className="left">
+        <div className="title">User</div>
+        <div className="counter">21312</div>
+      </div>
       <div className="right">Right</div>
     </div>
   );
